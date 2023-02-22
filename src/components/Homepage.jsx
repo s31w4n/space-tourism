b4c2344100342796511bd50a7ed35c1a7bb1a4fb
s31w4n <sewan.khalni@gmail.com>
2023-02-23 01:40:55 +0330

    remove some classes from homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -7,7 +7,7 @@ function Homepage() {
   document.body.classList.add("home");
 
   return (
-    <main id="main" className="grid-container grid-container--home">
+    <main className="grid-container grid-container--home">
       <div className="flow">
         <h1
           className="text-accent fs-500 ff-sans-cond uppercase letter-spacing-1"
@@ -21,7 +21,6 @@ function Homepage() {
             Space
           </span>
         </h1>
-
         <p>
           Let’s face it; if you want to go to space, you might as well genuinely
           go to outer space and not hover kind of on the edge of it. Well sit
